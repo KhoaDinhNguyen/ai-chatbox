c83a6e7c8c1952c65dd72a3b80f7b6b597ced024
Ngoc Dinh Khoa Nguyen <khoacode1305@gmail.com>
2026-07-22 18:14:57 -0700

    feat: update the favicon and metadata

--- backend/routes/chatboxRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import chatboxController from "../controllers/chatboxController.js";
-
-const chatboxRoute = express.Router();
-
-chatboxRoute.post("/", chatboxController.sendPrompt);
-
-export default chatboxRoute;
